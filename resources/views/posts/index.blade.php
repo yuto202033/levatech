da696b8aa9b322339d333049c4d09f7a803e3021
yuto202033 <takuyutohj@gmail.com>
2022-07-05 15:39:55 +0900

    [add]記事詳細画面

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -11,10 +11,15 @@
         <div class='posts'>
             @foreach ($posts as $post)
                 <div class='post'>
-                    <h2 class='title'>{{ $post->title }}</h2>
+                    <h2 class='title'>
+                         <a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
+                    </h2>
                     <p class='body'>{{ $post->body }}</p>
                 </div>
             @endforeach
+        </div>
+         <div class='paginate'>
+            {{ $posts->links() }}
         </div>
     </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,4 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', 'PostController@index');
+Route::get('/posts/{post}', 'PostController@show');
