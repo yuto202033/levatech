284ce9f0346fad49b0fea003e68f9a561856a2d8
yuto202033 <takuyutohj@gmail.com>
2022-07-08 20:07:33 +0900

    ブログ投稿作成実行

---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE HTML>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <title>Blog</title>
+    </head>
+    <body>
+        <h1>Blog Name</h1>
+        <form action="/posts" method="POST">
+            @csrf
+            <div class="title">
+                <h2>Title</h2>
+                <input type="text" name="post[title]" placeholder="タイトル"/>
+            </div>
+            <div class="body">
+                <h2>Body</h2>
+                <textarea name="post[body]" placeholder="今日も1日お疲れさまでした。"></textarea>
+            </div>
+            <input type="submit" value="store"/>
+        </form>
+        <div class="back">[<a href="/">back</a>]</div>
+    </body>
+</html>
